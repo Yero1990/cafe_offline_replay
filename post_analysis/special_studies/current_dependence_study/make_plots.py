762b1d321f48c087684e6de0f0a14ccd3ef65fe6
Carlos Yero <cyero@jlab.org>
2023-03-07 01:14:49 -0500

    updated slides

--- post_analysis/special_studies/current_dependence_study/make_plots.py
+++ post_analysis/special_studies/current_dependence_study/make_plots.py
@@ -12,7 +12,8 @@ def get_data(target='', kin='', header=''):
     # read any data from the .csv file
     
     # define summary file
-    summary_file_path = '../../summary_files/pass1_with_tcoin/tight_current_cut/cafe_prod_%s_%s_report_summary.csv' % (target, kin)
+    #summary_file_path = '../../summary_files/pass1_with_tcoin/tight_current_cut/cafe_prod_%s_%s_report_summary.csv' % (target, kin)
+    summary_file_path = '../../summary_files/pass1_with_tcoin/above_5uA_cut/cafe_prod_%s_%s_report_summary.csv' % (target, kin)
 
     # read .csv file
     df = pd.read_csv(summary_file_path, comment='#')
@@ -82,13 +83,16 @@ def make_plots():
 
     I_ca48_mf             = get_data('Ca48', 'MF', 'avg_current')
     Q_ca48_mf             = get_data('Ca48', 'MF', 'charge')
+    T2_scl_ca48_mf        = get_data('Ca48', 'MF', 'T2_scl_rate') * 1000. * get_data('Ca48', 'MF', 'beam_time')
     N_ca48_mf             = unumpy.uarray(get_data('Ca48', 'MF', 'real_Yield'),  get_data('Ca48', 'MF', 'real_Yield_err') )
     hms_trk_eff_ca48_mf   = unumpy.uarray(get_data('Ca48', 'MF', 'hTrkEff'),     get_data('Ca48', 'MF', 'hTrkEff_err') )
     shms_trk_eff_ca48_mf  = unumpy.uarray(get_data('Ca48', 'MF', 'pTrkEff'),     get_data('Ca48', 'MF', 'pTrkEff_err') )
     total_LT_ca48_mf      = unumpy.uarray(get_data('Ca48', 'MF', 'tLT'),         get_data('Ca48', 'MF', 'tLT_err_Bi') )
     mult_trk_eff_ca48_mf  = np.array(get_data('Ca48', 'MF', 'multi_track_eff') )
     Y_ca48_mf = N_ca48_mf / ( Q_ca48_mf * hms_trk_eff_ca48_mf * shms_trk_eff_ca48_mf * total_LT_ca48_mf * mult_trk_eff_ca48_mf)              
-    relY_ca48_mf  = Y_ca48_mf[-3:] / (Y_ca48_mf[-3:])[0]  
+    relY_ca48_mf  = Y_ca48_mf[-3:] / (Y_ca48_mf[-3:])[0]
+    T2_scl_norm_ca48_mf = T2_scl_ca48_mf / Q_ca48_mf
+    relT2_ca48_mf = T2_scl_norm_ca48_mf[-3:]  / (T2_scl_norm_ca48_mf[-3:])[0] 
 
     I_fe54_mf             = get_data('Fe54', 'MF', 'avg_current')
     Q_fe54_mf             = get_data('Fe54', 'MF', 'charge')
@@ -101,21 +105,25 @@ def make_plots():
     relY_fe54_mf  = Y_fe54_mf / Y_fe54_mf[0]  
 
     plt.subplot(1, 2, 1)
-    plt.errorbar(I_be9_mf,       unumpy.nominal_values(Y_be9_mf)/Y_be9_mf[0].n,             unumpy.std_devs(Y_be9_mf)/Y_be9_mf[0].n,             marker='o',markersize=8, color='r', mec='k', linestyle='dashed', label='Be9 MF (yield)')
-    plt.errorbar(I_b10_mf,       unumpy.nominal_values(Y_b10_mf)/Y_b10_mf[0].n,             unumpy.std_devs(Y_b10_mf)/Y_b10_mf[0].n,             marker='o',markersize=8, color='b', mec='k', linestyle='dashed', label='B10 MF (yield)')
-    plt.errorbar(I_b11_mf,       unumpy.nominal_values(Y_b11_mf)/Y_b11_mf[1].n,             unumpy.std_devs(Y_b11_mf)/Y_b11_mf[1].n,             marker='o',markersize=8, color='g', mec='k', linestyle='dashed', label='B11 MF (yield)')
-    plt.errorbar(I_c12_mf[-4:],       unumpy.nominal_values(Y_c12_mf[-4:])/(Y_c12_mf[-4:])[1].n,             unumpy.std_devs(Y_c12_mf[-4:])/(Y_c12_mf[-4:])[1].n,             marker='o',markersize=8, color='darkorange', mec='k', linestyle='dashed', label='C12 MF (yield)')
+    #plt.errorbar(I_be9_mf,       unumpy.nominal_values(Y_be9_mf)/Y_be9_mf[0].n,             unumpy.std_devs(Y_be9_mf)/Y_be9_mf[0].n,             marker='o',markersize=8, color='r', mec='k', linestyle='dashed', label='Be9 MF (yield)')
+    #plt.errorbar(I_b10_mf,       unumpy.nominal_values(Y_b10_mf)/Y_b10_mf[0].n,             unumpy.std_devs(Y_b10_mf)/Y_b10_mf[0].n,             marker='o',markersize=8, color='b', mec='k', linestyle='dashed', label='B10 MF (yield)')
+    #plt.errorbar(I_b11_mf,       unumpy.nominal_values(Y_b11_mf)/Y_b11_mf[1].n,             unumpy.std_devs(Y_b11_mf)/Y_b11_mf[1].n,             marker='o',markersize=8, color='g', mec='k', linestyle='dashed', label='B11 MF (yield)')
+    #plt.errorbar(I_c12_mf[-4:],       unumpy.nominal_values(Y_c12_mf[-4:])/(Y_c12_mf[-4:])[1].n,             unumpy.std_devs(Y_c12_mf[-4:])/(Y_c12_mf[-4:])[1].n,             marker='o',markersize=8, color='darkorange', mec='k', linestyle='dashed', label='C12 MF (yield)')
 
     print('I_c12_mf -->',I_c12_mf)
     print('Y_c12_mf --->', unumpy.nominal_values(Y_c12_mf))
-    
+    print('T2_scl_rate_norm_ca48_mf[-3:]-->',T2_scl_norm_ca48_mf[-3:])
     plt.legend()
 
     
     plt.subplot(1, 2, 2)
-    plt.errorbar(I_ca48_mf[-3:],  unumpy.nominal_values(Y_ca48_mf[-3:])/(Y_ca48_mf[-3:])[0].n, unumpy.std_devs(Y_ca48_mf[-3:])/(Y_ca48_mf[-3:])[0].n, marker='o',markersize=8, color='b', mec='k', linestyle='dashed', label='Ca48 MF (yield)')
-    plt.errorbar(I_ca40_mf,       unumpy.nominal_values(Y_ca40_mf)/Y_ca40_mf[0].n,             unumpy.std_devs(Y_ca40_mf)/Y_ca40_mf[0].n,             marker='o',markersize=8, color='g', mec='k', linestyle='dashed', label='Ca40 MF (yield)')
-    plt.errorbar(I_fe54_mf,       unumpy.nominal_values(Y_fe54_mf)/Y_fe54_mf[1].n,             unumpy.std_devs(Y_fe54_mf)/Y_fe54_mf[1].n,             marker='o',markersize=8, color='m', mec='k', linestyle='dashed', label='Fe54 MF (yield)')
+    plt.errorbar(I_ca48_mf[-3:],  unumpy.nominal_values(Y_ca48_mf[-3:])/(Y_ca48_mf[-3:])[0].n, unumpy.std_devs(Y_ca48_mf[-3:])/(Y_ca48_mf[-3:])[0].n, marker='o',markersize=8, color='magenta', mec='k', linestyle='dashed', label='Ca48 MF (yield)')
+    plt.plot(I_ca48_mf[-3:],  T2_scl_norm_ca48_mf[-3:]/(T2_scl_norm_ca48_mf[-3:])[0], marker='o',markersize=8, color='magenta', mec='k', linestyle='solid', label='Ca48 MF (T2 e- scalers)')
+    
+    plt.errorbar(I_ca40_mf,       unumpy.nominal_values(Y_ca40_mf)/Y_ca40_mf[0].n,             unumpy.std_devs(Y_ca40_mf)/Y_ca40_mf[0].n,             marker='o',markersize=8, color='darkorange', mec='k', linestyle='dashed', label='Ca40 MF (yield)')
+    plt.errorbar(I_fe54_mf,       unumpy.nominal_values(Y_fe54_mf)/Y_fe54_mf[1].n,             unumpy.std_devs(Y_fe54_mf)/Y_fe54_mf[1].n,             marker='o',markersize=8, color='gold', mec='k', linestyle='dashed', label='Fe54 MF (yield)')
+
+    plt.ylim(0.9, 1.07)
 
     plt.legend()
     plt.show()
