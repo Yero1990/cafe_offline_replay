da154f1f25c3f86cdf72dfdd5d00fef94db4d71c
Carlos Yero <cyero@jlab.org>
2023-06-04 14:38:34 -0400

    added pass3 csv files

--- post_analysis/scripts/plot_double_ratio.py
+++ post_analysis/scripts/plot_double_ratio.py
@@ -7,7 +7,18 @@ from uncertainties import ufloat
 from uncertainties import unumpy
 
 
-ifname= 'double_ratios_pass2.csv'
+if(len(sys.argv)!=2):
+    print('''\n
+    -----------------------------------------
+    Usage: ipython plot_double_ratio.py passN
+    -----------------------------------------
+    ''')
+    sys.exit()
+
+npass = sys.argv[1]
+
+
+ifname= 'double_ratios_%s.csv' % (npass)
 
 # read input file
 df = pd.read_csv(ifname, comment='#')
@@ -19,23 +30,143 @@ A = df['A']
 NoZ = df['NoZ'] 
 NmZoA = df['NmZoA'] 
 
-fig1 = plt.figure()
-plt.errorbar(A, double_ratio, double_ratio_err, marker='o', markersize=10, mfc='k', ecolor='k', mec='k', linestyle='None')
+compare_flag = True
+
+if(compare_flag):
+    fname= 'double_ratios_pass2.csv' 
+
+    # read input file
+    df = pd.read_csv(fname, comment='#')
+    
+    targ_2 = df['target']
+    double_ratio_2  = np.array(df['doubleR'])
+    double_ratio_err_2  = np.array(df['doubleR_err'])
+    A_2 = df['A'] 
+    NoZ_2 = df['NoZ'] 
+    NmZoA_2 = df['NmZoA'] 
+
+
+
+
+#--------------------------
+# PLOT Double Ratio vs. A
+#--------------------------
+
+fig1= plt.figure()
+plt.errorbar(A, double_ratio, double_ratio_err, marker='o', markersize=7, mfc='k', ecolor='k', mec='k', linestyle='None')
+if(compare_flag ):
+    plt.errorbar(A_2, double_ratio_2, double_ratio_err_2, marker='o', markersize=7, mfc='r', ecolor='r', mec='r', linestyle='None')
+    
 plt.xscale('log')
 plt.xlabel('A')
 plt.ylabel('A (SRC/MF) / C12 (SRC/MF)')
+# add target names to plot
+for i, tgt in enumerate(targ):
+    print('i, tgt -> ',i, tgt)
+    print('(x,y) ->  ',A[i], double_ratio[i] )
 
+    # rename without subscript for cleaner text display on canvas
+    if(tgt=="B10_corr"): tgt = "B10"
+    if(tgt=="B11_corr"): tgt = "B11"
+    if(tgt=="Ca48_corr"): tgt = "Ca48"
+    
+    
+    if(A[i]>15):
+        x = A[i] + 5
+        y = double_ratio[i]
+    else:
+        x = A[i] + 2
+        y = double_ratio[i]
+
+    if tgt=="Au197":
+        x = A[i] - 70
+    if tgt=="B11":
+        x = A[i] - 2
+        y = double_ratio[i] + 0.05
+    plt.text(x, y, tgt)
+
+
+
+#--------------------------
+# PLOT Double Ratio vs. N/Z
+#--------------------------
 fig2 = plt.figure()
-plt.errorbar(NoZ, double_ratio, double_ratio_err, marker='o', markersize=10, mfc='k', ecolor='k', mec='k', linestyle='None')
+plt.errorbar(NoZ, double_ratio, double_ratio_err, marker='o', markersize=7, mfc='k', ecolor='k', mec='k', linestyle='None')
+
+if(compare_flag ):
+    plt.errorbar(NoZ_2, double_ratio_2, double_ratio_err_2, marker='o', markersize=7, mfc='r', ecolor='r', mec='r', linestyle='None')
+
 plt.xlabel('N/Z')
 plt.ylabel('A (SRC/MF) / C12 (SRC/MF)')
+for i, tgt in enumerate(targ):
+    print('i, tgt -> ',i, tgt)
+    print('(x,y) ->  ',NoZ[i], double_ratio[i] )
+
+    # rename without subscript for cleaner text display on canvas
+    if(tgt=="B10_corr"): tgt = "B10"
+    if(tgt=="B11_corr"): tgt = "B11"
+    if(tgt=="Ca48_corr"): tgt = "Ca48"
+    
+    x = NoZ[i] + 0.01
+    y = double_ratio[i]
+
+    if tgt=="Au197":
+        x = NoZ[i] - 0.07
+ 
+    plt.text(x, y, tgt)
+
+
+
+
+
 
 fig3 = plt.figure()
-plt.errorbar(NmZoA, double_ratio, double_ratio_err, marker='o', markersize=10, mfc='k', ecolor='k', mec='k', linestyle='None')
+plt.errorbar(NmZoA, double_ratio, double_ratio_err, marker='o', markersize=7, mfc='k', ecolor='k', mec='k', linestyle='None')
+
+if(compare_flag ):
+    plt.errorbar(NmZoA_2, double_ratio_2, double_ratio_err_2, marker='o', markersize=7, mfc='r', ecolor='r', mec='r', linestyle='None')
+
 plt.xlabel('(N-Z)/A')
 plt.ylabel('A (SRC/MF) / C12 (SRC/MF)')
 
+for i, tgt in enumerate(targ):
+    print('i, tgt -> ',i, tgt)
+    print('(x,y) ->  ',NmZoA[i], double_ratio[i] )
+
+    # rename without subscript for cleaner text display on canvas
+    if(tgt=="B10_corr"): tgt = "B10"
+    if(tgt=="B11_corr"): tgt = "B11"
+    if(tgt=="Ca48_corr"): tgt = "Ca48"
+    
+    x = NmZoA[i] + 0.005
+    y = double_ratio[i]
+
+    if tgt=="Au197":
+        x = NmZoA[i] - 0.03
+ 
+    plt.text(x, y, tgt)
+
+
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 '''
 #print('double_ratio = ', double_ratio)
